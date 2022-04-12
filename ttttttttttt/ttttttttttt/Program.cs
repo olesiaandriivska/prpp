93565d9932629ede9dfccb34b34e0ffca0d4c365
olesiaandriivska <olesia.andriivska1346@gmail.com>
2022-04-12 18:12:26 +0300

    initial commit

---
+++ ttttttttttt/ttttttttttt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ttttttttttt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
