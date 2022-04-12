e9e9bc65fe423cf5a305ebc75c87b81a0974715d
YuriiKaras <55154470+YuriiKaras@users.noreply.github.com>
2022-04-12 17:54:05 +0300

    я написав свій float))))

--- lab2/lab2/lab2.cpp
+++ lab2/lab2/lab2.cpp
@@ -1,20 +1,22 @@
-// lab2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+
+typedef struct myfloat {
+    unsigned int mantissa : 23;
+    unsigned int exponent : 8;
+    unsigned int sign : 1;
+} myfloat_t;
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    float num = 0.025f;
+    myfloat_t shit = *(myfloat_t*)&num;
+    printf("Sign:\t%d\nexponent:\t%d\nmantissa:\t%d\n", shit.sign, shit.exponent, shit.mantissa);
+
+
+    // std::cout << "Hello World!\n";
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
