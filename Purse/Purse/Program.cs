c1f0e1fd24a3c42c3bbe83bad0599ee10f79815b
YuriiKaras <55154470+YuriiKaras@users.noreply.github.com>
2022-04-13 13:59:26 +0300

    Merged all end did smth

--- Purse/Purse/Program.cs
+++ Purse/Purse/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using NSMoney;
-
+using NSBusinessCard;
+using Cc;
 
 namespace Purse
 {
@@ -12,6 +13,16 @@ namespace Purse
             Money m = new Money(Money.Currency.USD, 200);
 
             m.PrintValue();
+
+            BusinessCard bs = new BusinessCard();
+
+            bs.BCFunc();
+
+
+            CreditCard _cc = new CreditCard();
+            CreditCard.Test();
+
+
         }
     }
 }
